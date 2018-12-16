557abc7949ea1a67ceb30b0927288e36bfe8d821
Alfred Qiu <sc941203@gmail.com>
2018-12-16 17:08:29 +0800

    modify

---
+++ src/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+import path from 'path';
+import Command from 'common-bin';
+import pkg from '../package.json';
+
+class MainCommand extends Command {
+  constructor(rawArgv) {
+    super(rawArgv);
+
+    this.usage = 'Usage: plutarch <command> [options]';
+
+    // load entire command directory
+    this.load(path.join(__dirname, 'commands'));
+
+    // more custom with `yargs` api, such as you can use `plutarch -V`
+    this.yargs.alias('v', 'version');
+  }
+
+  get version() {
+    return pkg.version || '1.0.0';
+  }
+}
+
+export default MainCommand;
