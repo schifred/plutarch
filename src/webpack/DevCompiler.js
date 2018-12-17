b4f051a7b0d2bdfb821e3f4dd6534e0fd81012d4
Alfred Qiu <sc941203@gmail.com>
2018-12-17 21:47:46 +0800

    modify

--- src/webpack/DevCompiler.js
+++ src/webpack/DevCompiler.js
@@ -1,11 +1,9 @@
 import webpack from 'webpack';
 import { override } from 'core-decorators';
 import WebpackDevServer from 'webpack-dev-server';
-import clearConsole from 'react-dev-utils/clearConsole';
 import openBrowser from 'react-dev-utils/openBrowser';
-import { choosePort, createCompiler, prepareProxy, prepareUrls } from 'react-dev-utils/WebpackDevServerUtils';
+import { choosePort, prepareUrls } from 'react-dev-utils/WebpackDevServerUtils';
 import errorOverlayMiddleware from 'react-dev-utils/errorOverlayMiddleware';
-import noopServiceWorkerMiddleware from 'react-dev-utils/noopServiceWorkerMiddleware';
 
 import BaseCompiler from './BaseCompiler';
 import logger from '../logger';
@@ -17,32 +15,36 @@ class DevCompiler extends BaseCompiler {
     const webpackConfig = await this.generate('development');
     const compiler = webpack(webpackConfig);
     const { context } = this;
-    const { devServer } = webpackConfig
+    const { devServer, open } = this.options || {};
     const { https, host = 'localhost', port = 3001 } = devServer || {};
     const protocol = https ? 'https' : 'http';
     const urls = prepareUrls(protocol, host, port);
     const client = new WebpackDevServer(compiler, {
-      ...devServer, 
       noInfo: true,
       inline: true,// 处理实时重载的 js 脚本以内联模式插入到页面中
       hot: true,// 模块热替换
       hotOnly: true,// 热替换时，编译失败时是否禁止刷新页面
+      ...devServer
     });
 
     // 模拟路由
     applyMockRoutes(client.app, context);
     client.use(errorOverlayMiddleware);
 
-    client.listen(port, host, err => {
-      if (err) {
-        logger.red("create dev server failed");
-        logger.log(err.stack || err.message);
-        return;
-      };
-  
-      logger.blue(`create dev server successful: http://${host}:${port}`);
-  
-      openBrowser(urls.localUrlForBrowser);
+    choosePort(host, port).then(realPort => {
+      client.listen(realPort, host, err => {
+        if (err) {
+          logger.red("create dev server failed");
+          logger.log(err.stack || err.message);
+          return;
+        };
+    
+        logger.blue(`create dev server successful: http://${host}:${realPort}`);
+    
+        if ( open && openBrowser(urls.localUrlForBrowser) ){
+          logger.log('The browser tab has been opened!');
+        };
+      });
     });
     
     ['SIGINT', 'SIGTERM'].forEach(sig => {
